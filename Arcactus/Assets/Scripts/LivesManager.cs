ed5e69785a9dfdbd36e1ac7018dc536740f71536
saskiabe <saskiabennecke@gmail.com>
2015-12-05 18:24:53 +0100

    Half Life Functionality

--- Arcactus/Assets/Scripts/LivesManager.cs
+++ Arcactus/Assets/Scripts/LivesManager.cs
@@ -11,6 +11,9 @@ public class LivesManager : MonoBehaviour {
     public RawImage life1;
     public RawImage life2;
     public RawImage life3;
+    public RawImage halflife1;
+    public RawImage halflife2;
+    public RawImage halflife3;
 
     private GameController gameController;
     private ScoreManager scoreManager;
@@ -50,8 +53,11 @@ public class LivesManager : MonoBehaviour {
 
     void UpdateLives()
     {
+        halflife1.enabled = (lives >= .5f && lives < 1);
         life1.enabled = (lives >= 1);
+        halflife2.enabled = (lives >= 1.5f && lives < 2);
         life2.enabled = (lives >= 2);
+        halflife3.enabled = (lives >= 2.5f && lives < 3);
         life3.enabled = (lives >= 3);
     }
 
