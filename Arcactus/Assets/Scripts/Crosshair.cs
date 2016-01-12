4a7f58208de7bb32582849a3611d10a0db81a145
StulleBrot <stulle.brot@yahoo.de>
2016-01-13 00:42:54 +0100

    Saving Main Scene :(

--- Arcactus/Assets/Scripts/Crosshair.cs
+++ Arcactus/Assets/Scripts/Crosshair.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class Crosshair : MonoBehaviour {
 
-    //public Camera CameraFacing;
     public OVRCameraRig CameraFacing;
     private Vector3 originalScale;
 
